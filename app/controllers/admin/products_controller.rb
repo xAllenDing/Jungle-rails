ffbcadc232f8493f238a4c74000f2be60507e87d
Allen Ding <allen.ding@hotmail.com>
2023-03-12 20:12:14 -0700

    add categories to admin but isnt functional

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
-  #ask mentor about auth bug
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], passowrd: ENV["ADMIN_PASSWORD"]
   def index
     @products = Product.order(id: :desc).all
   end
